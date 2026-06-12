4b32577444819c2089f4ad7cf987280593d7d349
Emmanuel Okwuosa <emmanuelokwuosa21@Emmanuels-MacBook-Pro-5.local>
2026-06-12 13:50:04 -0500

    Send grid API addition

--- src/main/java/portfolio/personal/service/MailService.java
+++ src/main/java/portfolio/personal/service/MailService.java
@@ -1,24 +1,37 @@
 package portfolio.personal.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import portfolio.personal.model.Message;
-import  org.springframework.mail.SimpleMailMessage;
-import  org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.stereotype.Service;
+import portfolio.personal.model.Message;
+
+import java.io.IOException;
 
 @Service
 public class MailService {
-    @Autowired
-    private JavaMailSender mailSender;
 
-    public void sendMessage(Message message){
-        SimpleMailMessage email = new SimpleMailMessage();
+    private final String apiKey = System.getenv("SENDGRID_API_KEY");
 
-        email.setTo("okwuosaemmanuel211@gmail.com");
-        email.setSubject(message.getSubject());
-        email.setText("From: " + message.getName() + " | " + message.getEmail() + "\n\n" + message.getMessage());
-        email.setReplyTo(message.getEmail());
-        mailSender.send((email));
+    public void sendMessage(Message message) throws IOException {
+        Email from = new Email("okwuosaemmanuel211@gmail.com");
+        Email to = new Email("okwuosaemmanuel211@gmail.com");
+        String subject = message.getSubject();
+        Content content = new Content("text/plain",
+                "From: " + message.getName() +
+                        "\nEmail: " + message.getEmail() +
+                        "\n\n" + message.getMessage());
 
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(apiKey);
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        Response response = sg.api(request);
+        System.out.println("SendGrid status: " + response.getStatusCode());
     }
 }
